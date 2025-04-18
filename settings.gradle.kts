5b2f223116ee5adde10f152f2e941f3ec19b26ee
nicklasRice <nicklasr63@gmail.com>
2025-04-18 08:37:44 -0400

    Project default files and karoo-ext dependency

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/hammerheadnav/karoo-ext")
+            credentials {
+                username = providers.gradleProperty("gpr.user").get()
+                password = providers.gradleProperty("gpr.key").get()
+            }
+        }
+    }
+}
+
+rootProject.name = "Connexx"
+include(":app")
